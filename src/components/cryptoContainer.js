8e934195f5a5eb67df4469c63dd7a2c7614e4971
aditya745 <thakuraditya72@yahoo.in>
2018-11-16 17:30:19 +0200

    fixed error

--- src/components/cryptoContainer.js
+++ src/components/cryptoContainer.js
@@ -35,8 +35,8 @@ class CryptoContainer extends Component {
                             <img src={`https://s2.coinmarketcap.com/static/img/coins/16x16/${cryptos.id}.png`} alt="cryptoImg" className="icon" />
                             <h4>{cryptos.name} ({cryptos.symbol})</h4>
                             <h4>Price: {cryptos.quotes.USD.price.toFixed(2)}</h4>
-                            <h4>Percent_Change_1h:</h4> <h5 style = {(cryptos.quotes.USD.percent_change_1h < 1) ? {color:"red"} : {color:"green"}}>{cryptos.quotes.USD.percent_change_1h}</h5>
-                            <h4>Percent_Change_24h:</h4> <h5 style = {(cryptos.quotes.USD.percent_change_24h < 1) ? {color:"red"} : {color:"green"}}>{cryptos.quotes.USD.percent_change_24h}</h5>
+                            <h4>Percent_Change_1h:</h4> <h5 style={(cryptos.quotes.USD.percent_change_1h < 1) ? { color: "red" } : { color: "green" }}>{cryptos.quotes.USD.percent_change_1h}</h5>
+                            <h4>Percent_Change_24h:</h4> <h5 style={(cryptos.quotes.USD.percent_change_24h < 1) ? { color: "red" } : { color: "green" }}>{cryptos.quotes.USD.percent_change_24h}</h5>
                         </div>
                     </div>
                 )
@@ -63,16 +63,16 @@ class CryptoContainer extends Component {
 
         return (
             <div>
-                <div className = "crypto-header">
-                    <div>    
+                <div className="crypto-header">
+                    <div>
                         <input name="crypto" type="text" value={this.state.crypto} onChange={this.updateSearch} />
                     </div>
                     <div>
-                        <button className = "btn1" onClick = {this.handleSortByPrice}>Sort by Price</button>
-                    </div>   
+                        <button className="btn1" onClick={this.handleSortByPrice}>Sort by Price</button>
+                    </div>
                     <div>
-                        <button className = "btn2" onClick = {this.handleSortByRank}>Sort by Rank</button>
-                    </div>                
+                        <button className="btn2" onClick={this.handleSortByRank}>Sort by Rank</button>
+                    </div>
                 </div>
                 <div className="crypto-container">
                     {allCryptos}
